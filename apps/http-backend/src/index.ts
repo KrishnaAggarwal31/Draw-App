ed2c9c24c852e4fbc6cf74bb6c3a804c36090a46
Krishna Aggarwal <krishnaaggarwalcool@gmail.com>
2025-07-24 00:08:11 +0530

    added signup endpoint

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -8,26 +8,40 @@ import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async(req, res) => {
     
-    const data = CreateUserSchema.safeParse(req.body);
-    if (!data.success){
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if (!parsedData.success){
         res.json({
             message: "Incorrect Inputs"
         })
         return;
     }
 
-    res.json({
-        userId: "123"
-    })
+    try {
+        await prismaClient.user.create({
+            data:{
+                email: parsedData.data?.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+
+        res.json({
+            userId: "123"
+        })
+    }catch(e){
+        res.status(411).json({
+            message: "User already exist with this username "
+        })
+    }
 
 })
 
 app.post("/signin", (req, res) =>{
 
-     const data = SigninSchema.safeParse(req.body);
-    if (!data.success){
+     const parsedData = SigninSchema.safeParse(req.body);
+    if (!parsedData.success){
         res.json({
             message: "Incorrect Inputs"
         })
@@ -46,8 +60,8 @@ app.post("/signin", (req, res) =>{
 })
 
 app.post("/room", middleware, (req, res) =>{
-    const data = CreateRoomSchema.safeParse(req.body);
-    if (!data.success){
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if (!parsedData.success){
         res.json({
             message: "Incorrect Inputs"
         })
